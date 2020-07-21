3b7899a6cbf5bd3d592a9675c30a0d0ee3c50610
Danial Bleile <danbleile@gmail.com>
2020-07-21 11:51:41 -0700

    Add Embed Template

--- functions.php
+++ functions.php
@@ -5,3 +5,38 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri(). '/style.css' ,
     array('parent-style'), wp_get_theme()->get('Version') );
 }
+
+function showcase_create_posttype() {
+ 
+    register_post_type( 'profile',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Profiles' ),
+                'singular_name' => __( 'Profile' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+			'rewrite' => array('slug' => 'profiles'),
+			'supports' => array('title', 'editor', 'custom-fields', 'excerpt', 'thumbnail'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+
+
+add_action( 'init', 'showcase_create_posttype' );
+
+
+function showcase_embed_template( $template ) {
+	
+	if ( isset( $_GET['modal-embed'] ) ) {
+		return __DIR__ . '/embed-template.php';
+	} 
+ 
+    return $template;
+}
+
+
+add_filter( 'template_include', 'showcase_embed_template' );
